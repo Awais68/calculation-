c5d20b587dbc3e1f5cad80caab9249d5be9d7000
Mr. Awais <161300531+Awais68@users.noreply.github.com>
2024-02-29 14:21:38 +0500

    f

---
+++ app.js
@@ -0,0 +1,106 @@
+alert("Hello! Well-Come to JavaScript Program")
+
+
+// Question No 1
+var x = 25
+var y = 7
+
+result  = (x % y) 
+
+console.log(result)
+
+//Q-2
+var a = 15
+var b = 4
+
+var multiply = (a * b)
+console.log(multiply)
+// after subtraction and addition 
+var a = 25
+var b = 2
+var multiply = (a * b)
+console.log(multiply)
+
+// Q-3
+var c = 12
+var d = 3
+var result = (c ** d)
+console.log(result)
+
+// Q-4
+var e = 10
+var f = 3
+
+var result = (e += f *2)
+console.log(result)
+
+// Q -5
+var g = 20
+var h = 4
+
+var result = (g / h)
+console.log(result)
+
+var g = 20
+var h = 4
+var result=(g + 5 /h)
+console.log(result)
+
+//Q-6
+var i = 8
+var j = 3
+
+var result=(i-2 * j)
+console.log(result)
+
+// Q-7
+var k = 18
+var l = 5
+var result = (k - l+2)
+console.log (result)
+// Q-8
+var m = 7
+var n = 2
+var result =(m ** n)
+console.log(result)
+// Q9
+var o = 13
+var p = 4
+var result = (o % (p+2))
+console.log(result)
+
+// Q-10
+var q = 16
+var r = 3
+var result =(q/2 * r)
+console.log(result)
+
+// Q-11
+var s = 25
+var t = 3
+var result = (s-1 / t)
+console.log(result)
+
+// Q -12
+var u = 11
+var v = 3
+var result = (u - v+2)
+console.log(result)
+
+//Q-13
+var w = 22
+var x = 5
+var result =(w + (x **2))
+console.log(result)
+
+//Q-14
+var y = 19
+var z = 4
+var result = (y % (z+1))
+console.log (result)
+
+//Q-15
+var a = 10
+var b = 3
+var result = (a * (b-2))
+console.log(result)
